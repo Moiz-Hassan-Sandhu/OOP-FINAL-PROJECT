fccf289af6c28bc0506c12a2132ee58c494d87be
sick_sandhu <i242096@isb.nu.edu.pk>
2025-05-01 17:27:28 +0500

    Revert "Reapply "deciding the structure and data members of the specific classes""
    This reverts commit ff4cbac68b90326c3e77be1f017c6b249dd79959.

--- main.cpp
+++ main.cpp
@@ -1,17 +1,5 @@
 #include<iostream>
 using namespace std;
-
-//super class
-
-//super class get inherited by each type of user i.e. manager
-/*policy engine also inherits the super class and we can do polymorphism 
-or what ever to access the data-members and the member-functions of each derived class*/  //polymorphims done
-
-//we have to decide which class will contain which data-members 
-
-
-
-
 int main()
 {
     cout<<endl<<endl<<"compiled sucessfully!"<<endl<<endl;
